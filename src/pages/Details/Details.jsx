4448d258034d01301c6748c81569101c827d3e33
Arthur Lopes <arthurlopr12@gmail.com>
2023-06-22 19:39:16 -0300

    section and tags components

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,13 +1,32 @@
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
 import { Header } from "../../components/Header/Header"
 import { Button } from "../../components/Button/Button"
+import { Section } from "../../components/Section/Section"
+import { Tag } from "../../components/Tag/Tag"
+
 
 export function Details() {
   // posso usar fragments tbm <> </> para subsituir a div
 
   return (
     <Container>
+
       <Header />
+
+      <Section title={"Links úteis"}>
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title={"Marcadores"}>
+        <Tag title={"express"} />
+        <Tag title={"nodejs"} />
+      </Section>
+
       <Button title="Voltar" />
 
     </Container>
